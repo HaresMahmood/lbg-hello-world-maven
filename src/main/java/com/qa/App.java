974fc6a2df09ff244532eb6e1a0d06ace01932d1
Hares Mahmood <hmahmood229@gmail.com>
2023-10-30 16:39:09 +0000

    Add line according to instructions

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Hares"));
         System.out.println(sayGoodbye());
     }
 
